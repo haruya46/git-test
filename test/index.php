3dee0e92c28fff07ab00c28e85761f618d0306d8
平山晴也 <puzu70@gmail.com>
2026-01-12 21:31:44 +0900

    フォームを追加

--- test/index.php
+++ test/index.php
@@ -9,4 +9,18 @@
 
 </p>
 
-<form action="test"></form>
+<form action="test" method="post">
+    <label for="name">名前</label>
+    <input type="text" id="name" name="name" required>
+
+    <label for="contact">連絡先</label>
+    <input type="text" id="contact" name="contact" required>
+
+    <label for="email">メール</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="message">お問い合わせ内容</label>
+    <textarea id="message" name="message" rows="5" required></textarea>
+
+    <button type="submit">送信</button>
+</form>
