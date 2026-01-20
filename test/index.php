28dae2b0c95279f04dfb40e95f3b17911f3c5d8f
平山晴也 <puzu70@gmail.com>
2026-01-20 18:13:09 +0900

    CSSを追加

--- test/index.php
+++ test/index.php
@@ -4,23 +4,4 @@
 <h1>これはテストです００１</h1>
 <h1>これはテストです００１</h1>
 <h1>これはテストです００１</h1>
-<p>
-    koreha
 
-</p>
-
-<form action="test" method="post">
-    <label for="name">名前</label>
-    <input type="text" id="name" name="name" required>
-
-    <label for="contact">連絡先</label>
-    <input type="text" id="contact" name="contact" required>
-
-    <label for="email">メール</label>
-    <input type="email" id="email" name="email" required>
-
-    <label for="message">お問い合わせ内容</label>
-    <textarea id="message" name="message" rows="5" required></textarea>
-
-    <button type="submit">送信</button>
-</form>
